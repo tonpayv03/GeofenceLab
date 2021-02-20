9206c44772ff2a1d16d6951af0096cae7cbcaba5
Rithnamrong.N <rithnamrong.n@gmail.com>
2021-02-21 04:19:23 +0700

    add Receiver

---
+++ GeofencingLab/GeofencingLab.Android/Receiver/BatteryLowReceiver.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GeofencingLab.Droid.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeofencingLab.Droid.Receiver
+{
+	//[BroadcastReceiver(Enabled = true, Exported = false, Name = Treasure.BATTERY_BROADCAST_RECEIVER)]
+	[BroadcastReceiver(Enabled = true, Exported = false)]
+	[IntentFilter(new[] { Android.Content.Intent.ActionBatteryLow })]
+	public class BatteryLowReceiver : BroadcastReceiver
+	{
+		private readonly string TAG = "BatteryLowReceiver";
+		public override void OnReceive(Context context, Intent intent)
+		{
+			NotificationHelper.PushHighNotification(context, TAG, "OnReceive");
+			Toast.MakeText(context, "Battery Low", ToastLength.Long).Show();
+		}
+	}
+}
---
+++ GeofencingLab/GeofencingLab.Android/Receiver/BootReceiver.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GeofencingLab.Dependency;
+using GeofencingLab.Droid.Helper;
+using GeofencingLab.Droid.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace GeofencingLab.Droid.Receiver
+{
+	[BroadcastReceiver(Enabled = true, Exported = false)]
+	[IntentFilter(new[] { Android.Content.Intent.ActionBootCompleted })]
+	public class BootReceiver : BroadcastReceiver
+	{
+		private static readonly string TAG = "BootReceiver";
+		public override void OnReceive(Context context, Intent intent)
+		{
+			NotificationHelper.PushHighNotification(context, TAG, "OnReceive");
+			if (MainActivity.Instance.CheckPermissionUserLocation() && MainActivity.Instance.CheckPermissionBackGroundLocation())
+			{
+				if (Treasure.IsGooglePlayServicesInstalled())
+				{
+					Intent locationIntent = new Intent(context, typeof(FusedLocationService));
+					context.StartService(locationIntent);
+
+					DependencyService.Get<IGeofencingManagerService>().InitGeofence();
+
+					NotificationHelper.PushHighNotification(context, TAG, "Initial Service Success");
+
+					CreateAlarmManager(context);
+				}
+			}
+		}
+
+		private void CreateAlarmManager(Context context)
+		{
+			var timeZone = 7;
+			var startTime = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day).AddHours(timeZone - 1).TimeOfDay; // alarm 8 โมงเช้า ครั้งต่อไป 2 ทุ่ม  // 23:00:00 PM UTC+7 = 6 โมงเช้าไทย
+
+			var elapsedTime = SystemClock.ElapsedRealtime();
+
+			AlarmManager alarmManager = context.GetSystemService(Context.AlarmService) as AlarmManager;
+
+			Preferences.Set(Treasure.ALARM_KEY, "StartService");
+			Intent alarmStartIntent = new Intent(context, typeof(AlarmFusedLocationReceiver));
+			var id = (int)SystemClock.ElapsedRealtime() + 1000;
+			PendingIntent pendingAlarmStartIntent = PendingIntent.GetBroadcast(context, id, alarmStartIntent, PendingIntentFlags.UpdateCurrent);
+
+			// 43200000 = 12 Hours
+			alarmManager.SetInexactRepeating(AlarmType.RtcWakeup, (long)startTime.TotalMilliseconds, AlarmManager.IntervalHalfDay, pendingAlarmStartIntent);
+		}
+	}
+}
