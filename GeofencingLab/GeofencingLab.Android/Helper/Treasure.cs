b34354e6b15127b619f7832700d3407c5597f869
Rithnamrong.N <rithnamrong.n@gmail.com>
2021-02-21 04:19:59 +0700

    add KEY

--- GeofencingLab/GeofencingLab.Android/Helper/Treasure.cs
+++ GeofencingLab/GeofencingLab.Android/Helper/Treasure.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Gms.Common;
 using Android.Gms.Maps.Model;
 using Android.OS;
 using Android.Runtime;
@@ -21,11 +22,18 @@ namespace GeofencingLab.Droid.Helper
 
 		public const string GEOFENCE_BROADCAST_RECEIVER = "GeofencingLab.Droid.Receiver.GeofencingBroadcastReceiver";
 		public const string LOCATION_BROADCAST_RECEIVER = "GeofencingLab.Droid.Receiver.LocationBroadcastReceiver";
+		public const string BATTERY_BROADCAST_RECEIVER = "GeofencingLab.Droid.Receiver.BatteryLowReceiver";
+		public const string ALARM_FUSED_BROADCAST_RECEIVER = "GeofencingLab.Droid.Receiver.AlarmFusedLocationReceiver";
+		public const string ALARM_END_FUSED_BROADCAST_RECEIVER = "GeofencingLab.Droid.Receiver.AlarmEndFusedLocationReceiver";
 
+		public const int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 10000;
 		public const int FINE_LOCATION_ACCESS_REQUEST_CODE = 10001;
 		public const int BACKGROUND_LOCATION_ACCESS_REQUEST_CODE = 10002;
 
 		public const string HIGH_CHANNEL_ID = "HIGH_NOTIFICATION_CHANNEL_ID";
+		public const string LOCATION_CHANNEL_ID = "LOCATION_NOTIFICATION_CHANNEL_ID";
+
+		public const string ALARM_KEY = "ALARM_KEY";
 
 		#region not const
 		public static int NOTIFY_ID = 0;
@@ -38,19 +46,25 @@ namespace GeofencingLab.Droid.Helper
 			{ "INTERCONTINENTAL", new LatLng (13.744704, 100.540939) },
 		};
 		#endregion
+
+		public static bool IsGooglePlayServicesInstalled()
+		{
+			var queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(Android.App.Application.Context);
+			if (queryResult == ConnectionResult.Success)
+			{
+				return true;
+			}
+
+			if (GoogleApiAvailability.Instance.IsUserResolvableError(queryResult))
+			{
+				var errorString = GoogleApiAvailability.Instance.GetErrorString(queryResult);
+				Toast.MakeText(Android.App.Application.Context, $"There is a problem with Google Play Services on this device: {queryResult} - {errorString}", ToastLength.Long).Show();
+			}
+
+			return false;
+		}
+
 	}
-
-	//public class LatLng
-	//{
-	//	public double lat { get; private set; }
-	//	public double lng;
-
-	//	public LatLng(double lat, double lng)
-	//	{
-	//		this.lat = lat;
-	//		this.lng = lng;
-	//	}
-	//}
 
 	// case key ซ้ำกัน แต่ value ต่างกัน
 	public class SiteListWithDuplicates : List<KeyValuePair<string, LatLng>>
@@ -60,5 +74,18 @@ namespace GeofencingLab.Droid.Helper
 			var element = new KeyValuePair<string, LatLng>(key, value);
 			this.Add(element);
 		}
+	}
+
+	
+	public enum MarkerIcon
+	{
+		Blue = 1,
+		LimeBlue = 2,
+		Navy = 3,
+		Orange = 4,
+		Purple = 5,
+		Red = 6,
+		Yellow = 7,
+		Gray = 8
 	}
 }
